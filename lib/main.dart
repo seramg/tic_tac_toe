f085763be47df6e80b7d97036d555a229356a158
Sera Mariam George <leelasera19@gmail.com>
2022-10-24 01:35:33 +0530

    Added the basic layout for the game

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/ui/theme/color.dart';
+import 'package:tic_tac_toe/utils/game_logic.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: TicTac(),
     );
   }
@@ -25,9 +26,21 @@ class TicTac extends StatefulWidget {
 }
 
 class _TicTacState extends State<TicTac> {
-  static const String turn = 'X';
+  static  String turn = 'X';
+
+  Game game = Game();
+  bool gameover= false;
+
+  @override
+  void initState() {
+    super.initState();
+    game.board = Game.initGameBoard();
+    print(game.board);
+  }
+
   @override
   Widget build(BuildContext context) {
+    double boardWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: MainColor.primaryColor,
       body: Column(
@@ -39,13 +52,59 @@ class _TicTacState extends State<TicTac> {
             'IT\'S $turn TURN'.toUpperCase(),
             style: const TextStyle(
               color: Colors.white,
-              fontSize: 75
+              fontSize: 55,
             ),
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           Container(
-            
-          )
+            width: boardWidth,
+            height: boardWidth,
+            child: GridView.count(
+              crossAxisCount: Game.boardlength ~/ 3,
+              //~ operator divides to integer and returns int value
+              padding: EdgeInsets.all(16),
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+              children: List.generate(Game.boardlength, (index) {
+                return GestureDetector(
+                  onTap: gameover?null:(){
+                    if(game.board![index]=="") {
+                            setState(() {
+                              game.board![index] = turn;
+                              if (turn == 'X') {
+                                turn = "O";
+                              } else {
+                                turn = "X";
+                              }
+                            });
+                          }
+
+                    },
+                  child: Container(
+                    width: Game.squaresize,
+                    height: Game.squaresize,
+                    decoration: BoxDecoration(
+                      color: MainColor.secondaryColor,
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: Center(
+                        child: Text(
+                      game.board![index],
+                      style: TextStyle(
+                          color: game.board![index] == 'X'
+                              ? MainColor.xcolor
+                              : MainColor.ocolor,
+                          fontSize:64.0,)
+                    )),
+                  ),
+                  
+                );
+              }),
+            ),
+          ),
+
         ],
       ),
     );
--- lib/ui/theme/color.dart
+++ lib/ui/theme/color.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MainColor{
-  static Color primaryColor= Color(0xff20013a);
-  static Color secondaryColor= Color(0xff0014a6);
+  static Color primaryColor= Color(0xff110938);
+  static Color secondaryColor= Color(0xff091f8e);
   static Color accentColor= Color(0xff1230e0);
-
+  static Color xcolor= Color(0xffd1d3de);
+  static Color ocolor= Color(0xffde6b8c);
 }
